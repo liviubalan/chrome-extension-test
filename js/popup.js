971f90b1e98185d4d8a1b05dbb225224c0163b43
Liviu Balan <liv_romania@yahoo.com>
2016-11-26 20:44:50 +0200

    Experiment with chrome.i18n JavaScript API

--- js/popup.js
+++ js/popup.js
@@ -1,2 +1,4 @@
-console.log('Translation for manifest_name');
-console.log(chrome.i18n.getMessage('manifest_name'));
+console.warn("i18n (translation)");
+console.log(chrome.i18n.getMessage("manifest_name"));
+console.log(chrome.i18n.getMessage("popup_bye", ["John", "wikipedia.org"]));
+console.log(chrome.i18n.getUILanguage());
