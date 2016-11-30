a4c187e47cd01fd8c49d29573efaa9f91de0d911
Liviu Balan <liv_romania@yahoo.com>
2016-11-30 18:26:01 +0200

    Experiment with optional_permissions..spellcheck manifest options

--- js/popup-browser_action.js
+++ js/popup-browser_action.js
@@ -2,3 +2,20 @@ console.warn("i18n (translation)");
 console.log(chrome.i18n.getMessage("manifest_name"));
 console.log(chrome.i18n.getMessage("popup_bye", ["John", "wikipedia.org"]));
 console.log(chrome.i18n.getUILanguage());
+
+$(document).ready(function () {
+    // Request optional permissions on mouse click
+    $("#emg-tabs").click(function () {
+        chrome.permissions.request({
+            permissions: ["tabs"],
+            origins: ["http://www.google.com/"]
+        }, function (granted) {
+            // The callback argument will be true if the user granted the permissions.
+            if (granted) {
+                alert("granted");
+            } else {
+                alert("not granted");
+            }
+        });
+    });
+});
